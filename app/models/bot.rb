69d3830dfb5ad353cb604fe781c08bbbde18fce6
Charles <giardina.charles@gmail.com>
2015-01-30 10:14:53 -0800

    the bots talk to EACH OTHER

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,13 +1,18 @@
 require 'cleverbot-api'
+require 'tts'
 
 bot_1 = CleverBot.new
 bot_2 = CleverBot.new
+
+
 
 current = ''
 loop do
   current = bot_1.think(current)
   puts "Bot 1: #{current}"
+  current.play('fr')
 
   current = bot_2.think(current)
   puts "Bot 2: #{current}"
+  current.play
 end
